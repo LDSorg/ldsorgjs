c03ebf3f774dc54de20955b4cca62c0642b0355e
AJ ONeal <coolaj86@gmail.com>
2014-04-03 13:23:24 -0600

    updated to latest

--- ldsorg.all.js
+++ ldsorg.all.js
@@ -2,6 +2,55 @@
 ;(function (exports) {
   'use strict';
 
+  // should be more like sequence than join
+  function forAllAsync(arr, fn, nThreads) {
+    // how many threads are allowed
+    nThreads = nThreads || 4;
+
+    var cbs = []
+      // how many threads are underway
+      , begun = 0
+      // how many have been run
+      , finished = 0
+      ;
+
+    function onFinishedBound() {
+      finished += 1;
+      onNextBound();
+    }
+    function onNextBound() {
+      if (0 === arr.length && finished === begun) {
+        complete();
+      }
+      while (arr.length && (begun - finished) < nThreads) {
+        begun += 1;
+        fn(onFinishedBound, arr.shift(), begun - 1, arr);
+      }
+    }
+    function complete() {
+      cbs.forEach(function (cb) {
+        cb();
+      });
+      cbs = [];
+    }
+
+    onNextBound();
+
+    return {
+      then: function (cb) {
+        cbs.push(cb);
+        onNextBound();
+        return this;
+      }
+    };
+  }
+
+  exports.forAllAsync = forAllAsync;
+}('undefined' !== typeof exports && exports || new Function('return this')()));
+/*jshint -W054 */
+;(function (exports) {
+  'use strict';
+
   function forEachAsync(arr, fn, thisArg) {
     var dones = []
       , index = -1
